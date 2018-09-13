202f0a4a182abfbc437314b29a80792412daf470
Antony Raphel <antonypr717@gmail.com>
2018-09-13 13:17:28 +0530

    pod update changes

--- ARCarMovement/ARCarMovement/ARCarMovement.swift
+++ ARCarMovement/ARCarMovement/ARCarMovement.swift
@@ -2,7 +2,7 @@
 //  ARCarMovement.swift
 //  ARCarMovement
 //
-//  Created by Mac05 on 24/10/17.
+//  Created by Antony Raphel on 24/10/17.
 //  Copyright © 2017 Antony Raphel. All rights reserved.
 //
 
@@ -28,32 +28,32 @@ extension FloatingPoint {
     func ARCarMovementMoved(_ Marker: GMSMarker)
 }
 
-public class ARCarMovement: NSObject {
-
+@objcMembers public class ARCarMovement: NSObject {
+    
     // MARK: Public properties
     public weak var delegate: ARCarMovementDelegate?
     public var duration: Float = 2.0
     
     public func ARCarMovement(marker: GMSMarker, oldCoordinate: CLLocationCoordinate2D, newCoordinate:CLLocationCoordinate2D, mapView: GMSMapView, bearing: Float) {
-    
+        
         //calculate the bearing value from old and new coordinates
         //
         let calBearing: Float = getHeadingForDirection(fromCoordinate: oldCoordinate, toCoordinate: newCoordinate)
         marker.groundAnchor = CGPoint(x: CGFloat(0.5), y: CGFloat(0.5))
         marker.rotation = CLLocationDegrees(calBearing); //found bearing value by calculation when marker add
         marker.position = oldCoordinate; //this can be old position to make car movement to new position
-    
+        
         //marker movement animation
         CATransaction.begin()
         CATransaction.setValue(duration, forKey: kCATransactionAnimationDuration)
         CATransaction.setCompletionBlock({() -> Void in
             marker.rotation = (Int(bearing) != 0) ? CLLocationDegrees(bearing) : CLLocationDegrees(calBearing)
         })
-
+        
         // delegate method pass value
         //
         delegate?.ARCarMovementMoved(marker)
-    
+        
         marker.position = newCoordinate; //this can be new position after car moved from old position to new position with animation
         marker.map = mapView;
         marker.rotation = CLLocationDegrees(calBearing);
