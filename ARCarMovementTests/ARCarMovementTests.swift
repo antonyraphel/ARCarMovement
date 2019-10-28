7eaf5c8de8d90cf435bf0282ad7470038613e497
Antony Raphel <antonyraphel@users.noreply.github.com>
2019-10-28 18:51:37 +0530

    GoogleMaps version (#20)

--- ARCarMovementTests/ARCarMovementTests.swift
+++ ARCarMovementTests/ARCarMovementTests.swift
@@ -45,13 +45,6 @@ class ARCarMovementTests: XCTestCase {
                                mapView: mView)
         
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 }
 
 extension ARCarMovementTests: ARCarMovementDelegate {
